4757d107acc00ed5894a8a90bc227fb7e42214d1
Mpho Mbonani <MMbonani@iqbusiness.net>
2020-06-17 21:52:04 +0200

    added and displaying datepicker

--- lib/widgets/Transactions/transactionsCreation.dart
+++ lib/widgets/Transactions/transactionsCreation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionsCreation extends StatefulWidget {
   final Function createTransaction;
@@ -8,16 +9,32 @@ class TransactionsCreation extends StatefulWidget {
 }
 
 class _TransactionsCreationState extends State<TransactionsCreation> {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
   void submitData() {
-    final title = titleController.text;
-    final amount = double.parse(amountController.text);
+    final title = _titleController.text;
+    final amount = double.parse(_amountController.text);
     if (title.isNotEmpty && amount > 0) {
       widget.createTransaction(title, amount);
       Navigator.of(context).pop();
     }
+  }
+
+  void _openDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2015),
+            lastDate: DateTime.now())
+        .then((selectedDate) {
+      if (selectedDate != null) {
+        setState(() {
+          _selectedDate = selectedDate;
+        });
+      }
+    });
   }
 
   @override
@@ -31,27 +48,31 @@ class _TransactionsCreationState extends State<TransactionsCreation> {
               children: <Widget>[
                 TextField(
                   decoration: InputDecoration(labelText: 'Title'),
-                  controller: titleController,
+                  controller: _titleController,
                   onSubmitted: (_) => submitData(),
                 ),
                 TextField(
                   decoration: InputDecoration(labelText: 'Amount'),
-                  controller: amountController,
+                  controller: _amountController,
                   keyboardType: TextInputType.number,
                   onSubmitted: (_) => submitData(),
                 ),
                 Container(
                   height: 70,
                   child: Row(children: <Widget>[
-                    Text('No Date Selected'),
+                    Text(_selectedDate == null
+                        ? 'No Date Selected'
+                        : DateFormat.yMd().format(_selectedDate)),
                     FlatButton(
-                      onPressed: (){},
-                      child: Text('Select Date', style: TextStyle(fontWeight: FontWeight.bold)),
-                      textColor: Theme.of(context).primaryColorDark)
+                        onPressed: _openDatePicker,
+                        child: Text('Select Date',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        textColor: Theme.of(context).primaryColorDark)
                   ]),
                 ),
                 FlatButton(
-                  child: Text('Add Transaction', style: TextStyle(fontWeight: FontWeight.bold)),
+                  child: Text('Add Transaction',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
                   textColor: Theme.of(context).primaryColorDark,
                   onPressed: submitData,
                   // there's something wrong here, button adds null transaction before method is called
