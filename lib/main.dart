d9c178e8c3287ecb9d121462b49182bcbdfb5114
Mpho Mbonani <MMbonani@iqbusiness.net>
2020-06-19 20:46:04 +0200

    removed dark theme. works better with light

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Expenses',
-      home: Primary(),
-      theme: ThemeData(
-        backgroundColor: Colors.white,
-        primarySwatch: Colors.yellow,
-        accentColor: Colors.black,
-        appBarTheme:
-            ThemeData.light().appBarTheme.copyWith(color: Colors.white),
-        fontFamily: 'OpenSans',
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      darkTheme: ThemeData(
-        backgroundColor: Colors.black,
-        primarySwatch: Colors.yellow,
-        accentColor: Colors.white,
-        appBarTheme: ThemeData.dark().appBarTheme.copyWith(color: Colors.black),
-        fontFamily: 'OpenSans',
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-    );
+        title: 'Expenses',
+        home: Primary(),
+        theme: ThemeData(
+          backgroundColor: Colors.white,
+          primarySwatch: Colors.yellow,
+          accentColor: Colors.black,
+          appBarTheme:
+              ThemeData.light().appBarTheme.copyWith(color: Colors.white),
+          fontFamily: 'OpenSans',
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ));
   }
 }
